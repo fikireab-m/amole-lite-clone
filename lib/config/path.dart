bf75af757b0091ab79415b9cafb2126c66e91809
fikireab-m <fikireab.meku@gmail.com>
2023-12-17 22:26:31 +0300

    appbar of login

---
+++ lib/config/path.dart
@@ -0,0 +1,4 @@
+class Images {
+  static const appIcon = 'assets/images/appicon.png';
+  static const dashenBank = 'assets/images/dashenbank.png';
+}
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,3 +1,4 @@
+import 'package:amole_lite/config/path.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -10,6 +11,77 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final sc = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 160.0,
+            width: sc.width,
+            decoration: const BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  color: Color(0xFF003CFF),
+                  width: 2.0,
+                ),
+              ),
+            ),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      Images.dashenBank,
+                      fit: BoxFit.scaleDown,
+                      width: 120.0,
+                      height: 120.0,
+                    ),
+                    Image.asset(
+                      Images.appIcon,
+                      fit: BoxFit.scaleDown,
+                      width: 120.0,
+                      height: 120.0,
+                    ),
+                    const SizedBox(
+                      width: 116,
+                      child: DefaultTextStyle(
+                        style: TextStyle(color: Color(0xFF003CFF)),
+                        child: Column(
+                          children: [
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: Text(
+                                'አሞሌ',
+                                style: TextStyle(fontSize: 48.0),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: Text(
+                                'AMOLE',
+                                style: TextStyle(fontSize: 18.0),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: Text(
+                                'Powered by DASHEN BANK',
+                                style: TextStyle(fontSize: 8.0),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
