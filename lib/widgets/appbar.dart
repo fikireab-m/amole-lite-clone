f56cc89718cb8d77a709b6e3caee7f180ae2c3e0
fikireab-m <fikireab.meku@gmail.com>
2023-12-19 22:29:15 +0300

    widgets +

---
+++ lib/widgets/appbar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AmoleAppBar extends StatelessWidget {
+  const AmoleAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar();
+  }
+}
---
+++ lib/widgets/bottom_navbar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AmoleBottomNavBar extends StatelessWidget {
+  const AmoleBottomNavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(items: []);
+  }
+}
