a4053a812c60c14bac96f33af943803d943df6a7
fikireab-m <fikireab.meku@gmail.com>
2023-12-28 20:45:08 +0300

    general appbar

--- lib/widgets/appbar.dart
+++ lib/widgets/appbar.dart
@@ -1,10 +1,31 @@
+import 'package:amole_lite/config/path.dart';
 import 'package:flutter/material.dart';
 
 class AmoleAppBar extends StatelessWidget {
-  const AmoleAppBar({super.key});
+  final List<Widget> appbarActions;
+  const AmoleAppBar({super.key, required this.appbarActions});
 
   @override
   Widget build(BuildContext context) {
-    return AppBar();
+    return AppBar(
+      title: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Image.asset(
+            Images.dashenBank,
+            fit: BoxFit.scaleDown,
+            height: 56.0,
+          ),
+          const SizedBox(width: 8.0),
+          Image.asset(
+            Images.amoleLogo,
+            fit: BoxFit.scaleDown,
+            height: 56.0,
+          ),
+        ],
+      ),
+      actions: appbarActions,
+    );
   }
 }
