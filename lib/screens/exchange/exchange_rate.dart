078479c393c3f8414de6e15c850eff31157889de
fikireab-m <fikireab.meku@gmail.com>
2024-01-06 16:47:59 +0300

    exchage rate page fix

--- lib/screens/exchange/exchange_rate.dart
+++ lib/screens/exchange/exchange_rate.dart
@@ -14,90 +14,104 @@ class ExchageRate extends StatelessWidget {
         body: Stack(
           children: [
             Padding(
-              padding: const EdgeInsets.fromLTRB(16.0, 132.0, 16.0, 4.0),
+              padding: const EdgeInsets.fromLTRB(16.0, 116.0, 16.0, 0.0),
               child: SingleChildScrollView(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     SizedBox(
-                      height: sc.height,
-                      child: ListView.separated(
-                        scrollDirection: Axis.vertical,
-                        itemBuilder: (context, i) {
-                          return i == 0
-                              ? Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    SizedBox(
-                                      height: 48,
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          const Text(
-                                              'Ethiopian Birr Exchange Rates'),
-                                          Text(
-                                              'Rates Updated ${DateTime.now()}')
-                                        ],
-                                      ),
-                                    ),
-                                    const Padding(
-                                      padding: EdgeInsets.only(top: 24.0),
-                                      child: DefaultTextStyle(
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.w700,
-                                          fontSize: 18.0,
-                                          color: ColorConst.darkTextColor,
-                                        ),
-                                        child: Row(
+                      height: sc.height - 200,
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 16.0, bottom: 4.0),
+                        child: ListView.builder(
+                          scrollDirection: Axis.vertical,
+                          itemBuilder: (context, i) {
+                            return i == 0
+                                ? Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      SizedBox(
+                                        height: 48,
+                                        child: Column(
                                           mainAxisAlignment:
                                               MainAxisAlignment.spaceBetween,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
                                           children: [
-                                            Text("CURRENCY"),
-                                            Text("BUY"),
-                                            Text("SELL"),
+                                            const Text(
+                                                'Ethiopian Birr Exchange Rates'),
+                                            Text(
+                                                'Rates Updated ${DateTime.now()}')
                                           ],
                                         ),
                                       ),
+                                      const Padding(
+                                        padding: EdgeInsets.only(top: 24.0),
+                                        child: DefaultTextStyle(
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w700,
+                                            fontSize: 18.0,
+                                            color: ColorConst.darkTextColor,
+                                          ),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text("CURRENCY"),
+                                              Text("BUY"),
+                                              Text("SELL"),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                      const Divider(height: 8.0)
+                                    ],
+                                  )
+                                : DefaultTextStyle(
+                                    style: const TextStyle(
+                                      fontSize: 16.0,
+                                      color: ColorConst.darkTextColor,
                                     ),
-                                  ],
-                                )
-                              : DefaultTextStyle(
-                                  style: const TextStyle(
-                                    fontSize: 16.0,
-                                    color: ColorConst.darkTextColor,
-                                  ),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Row(
-                                        mainAxisSize: MainAxisSize.min,
+                                    child: Container(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 8.0),
+                                      decoration: const BoxDecoration(
+                                        border: Border(
+                                          bottom: BorderSide(
+                                              width: 1.0,
+                                              color: ColorConst
+                                                  .lightGrayBackground),
+                                        ),
+                                      ),
+                                      child: Row(
                                         mainAxisAlignment:
                                             MainAxisAlignment.spaceBetween,
                                         children: [
-                                          CountryFlag.fromCountryCode(
-                                            'AE',
-                                            height: 32,
-                                            width: 32,
-                                            borderRadius: 8,
+                                          Row(
+                                            mainAxisSize: MainAxisSize.min,
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              CountryFlag.fromCountryCode(
+                                                'AE',
+                                                height: 32,
+                                                width: 32,
+                                                borderRadius: 8,
+                                              ),
+                                              const SizedBox(width: 16.0),
+                                              const Text("AED"),
+                                            ],
                                           ),
-                                          const SizedBox(width: 16.0),
-                                          const Text("AED"),
+                                          const Text("13.8092"),
+                                          const Text("14.08538"),
                                         ],
                                       ),
-                                      const Text("13.8092"),
-                                      const Text("14.08538"),
-                                    ],
-                                  ),
-                                );
-                        },
-                        separatorBuilder: (context, i) {
-                          return Divider(height: i == 0 ? 24.0 : 16.0);
-                        },
-                        itemCount: 100,
+                                    ),
+                                  );
+                          },
+                          itemCount: 24,
+                        ),
                       ),
                     ),
                   ],
@@ -106,7 +120,7 @@ class ExchageRate extends StatelessWidget {
             ),
             Column(
               children: [
-                const SizedBox(height: 56.0, child: AmoleAppBar()),
+                const SizedBox(height: 64.0, child: AmoleAppBar()),
                 const Divider(
                   height: 2,
                   thickness: 2.0,
