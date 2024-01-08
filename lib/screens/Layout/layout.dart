ac7ae5898989ad4e4bb8b0a89a373eb1c9829bd0
fikireab-m <fikireab.meku@gmail.com>
2024-01-08 11:53:16 +0300

    common layout

--- lib/screens/Layout/layout.dart
+++ lib/screens/Layout/layout.dart
@@ -1,17 +1,48 @@
+import 'package:amole_lite/constants/const_colors.dart';
+import 'package:amole_lite/screens/widgets/appbar.dart';
 import 'package:flutter/material.dart';
 
-class Layout extends StatefulWidget {
-  const Layout({super.key});
+class PageLayout extends StatelessWidget {
+  final Widget child;
+  const PageLayout({super.key, required this.child});
 
-  @override
-  State<Layout> createState() => _LayoutState();
-}
-
-class _LayoutState extends State<Layout> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 116.0, 16.0, 0.0),
+              child: SingleChildScrollView(
+                child: child,
+              ),
+            ),
+            Column(
+              children: [
+                const SizedBox(height: 64.0, child: AmoleAppBar()),
+                const Divider(
+                  height: 2,
+                  thickness: 2.0,
+                  color: ColorConst.primaryColor,
+                  indent: 0,
+                ),
+                AppBar(
+                  backgroundColor: ColorConst.lightBackground,
+                  surfaceTintColor: ColorConst.lightBackground,
+                  shadowColor: ColorConst.lightBlueGrayBackground,
+                  elevation: 2.0,
+                  leading: GestureDetector(
+                    onTap: () {},
+                    child: const Icon(Icons.arrow_back_ios),
+                  ),
+                  title: const Text('Exchange Rates'),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/screens/exchange/exchange_rate.dart
+++ lib/screens/exchange/exchange_rate.dart
@@ -1,6 +1,6 @@
 import 'package:amole_lite/constants/const_colors.dart';
+import 'package:amole_lite/screens/Layout/layout.dart';
 import 'package:amole_lite/screens/exchange/widgets/exchange_row.dart';
-import 'package:amole_lite/screens/widgets/appbar.dart';
 import 'package:flutter/material.dart';
 
 class ExchageRate extends StatelessWidget {
@@ -9,100 +9,58 @@ class ExchageRate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final sc = MediaQuery.of(context).size;
-    return SafeArea(
-      child: Scaffold(
-        body: Stack(
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(16.0, 116.0, 16.0, 0.0),
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      height: sc.height - 200,
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 16.0, bottom: 4.0),
-                        child: ListView.builder(
-                          scrollDirection: Axis.vertical,
-                          itemBuilder: (context, i) {
-                            return i == 0
-                                ? Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      SizedBox(
-                                        height: 48,
-                                        child: Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceBetween,
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            const Text(
-                                              'Ethiopian Birr Exchange Rates',
-                                            ),
-                                            Text(
-                                              'Rates Updated ${DateTime.now()}',
-                                            )
-                                          ],
-                                        ),
-                                      ),
-                                      const Padding(
-                                        padding: EdgeInsets.only(top: 24.0),
-                                        child: DefaultTextStyle(
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.w700,
-                                            fontSize: 18.0,
-                                            color: ColorConst.darkTextColor,
-                                          ),
-                                          child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.spaceBetween,
-                                            children: [
-                                              Text("CURRENCY"),
-                                              Text("BUY"),
-                                              Text("SELL"),
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                      const Divider(height: 8.0)
-                                    ],
-                                  )
-                                : const ExchangeRow();
-                          },
-                          itemCount: 24,
+    return PageLayout(
+      child: SizedBox(
+        height: sc.height - 200,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 16.0, bottom: 4.0),
+          child: ListView.builder(
+            scrollDirection: Axis.vertical,
+            itemBuilder: (context, i) {
+              return i == 0
+                  ? Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 48,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'Ethiopian Birr Exchange Rates',
+                              ),
+                              Text(
+                                'Rates Updated ${DateTime.now()}',
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Column(
-              children: [
-                const SizedBox(height: 64.0, child: AmoleAppBar()),
-                const Divider(
-                  height: 2,
-                  thickness: 2.0,
-                  color: ColorConst.primaryColor,
-                  indent: 0,
-                ),
-                AppBar(
-                  backgroundColor: ColorConst.lightBackground,
-                  surfaceTintColor: ColorConst.lightBackground,
-                  shadowColor: ColorConst.lightBlueGrayBackground,
-                  elevation: 2.0,
-                  leading: GestureDetector(
-                    onTap: () {},
-                    child: const Icon(Icons.arrow_back_ios),
-                  ),
-                  title: const Text('Exchange Rates'),
-                ),
-              ],
-            )
-          ],
+                        const Padding(
+                          padding: EdgeInsets.only(top: 24.0),
+                          child: DefaultTextStyle(
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 18.0,
+                              color: ColorConst.darkTextColor,
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text("CURRENCY"),
+                                Text("BUY"),
+                                Text("SELL"),
+                              ],
+                            ),
+                          ),
+                        ),
+                        const Divider(height: 8.0)
+                      ],
+                    )
+                  : const ExchangeRow();
+            },
+            itemCount: 24,
+          ),
         ),
       ),
     );
